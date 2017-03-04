36ee745578aad68f7df49a863df48281c3ee7e01
Eugene Wang <eugene.r.w.12@gmail.com>
2017-03-04 13:54:04 -0500

    added all the shit

--- app/src/main/java/com/silpe/vire/slip/MainActivity.java
+++ app/src/main/java/com/silpe/vire/slip/MainActivity.java
@@ -8,15 +8,45 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    LinearLayout tabHost;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        TabHost host = (TabHost)findViewById(R.id.tabHost);
+
         setSupportActionBar(toolbar);
+
+
+
+        host.setup();
+
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("Tab One");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Tab One");
+        host.addTab(spec);
+
+        //Tab 2
+        spec = host.newTabSpec("Tab Two");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Tab Two");
+        host.addTab(spec);
+
+        //Tab 3
+        spec = host.newTabSpec("Tab Three");
+        spec.setContent(R.id.tab3);
+        spec.setIndicator("Tab Three");
+        host.addTab(spec);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
