c34d8ffe8e0a3a859343b3a85ab96b7e436da7fc
Jeff Niu <jeffniu22@gmail.com>
2017-03-09 01:36:05 -0500

    xd

--- app/src/main/java/com/silpe/vire/slip/fragments/QRFragment.java
+++ app/src/main/java/com/silpe/vire/slip/fragments/QRFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.silpe.vire.slip.R;
 
@@ -39,7 +40,11 @@ public class QRFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_qr, container, false);
+        View view = inflater.inflate(R.layout.fragment_qr, container, false);
+        TextView uidText = (TextView) view.findViewById(R.id.camera_uid);
+        uidText.setText(uid);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
