cb67d8d0549753db12826a4f79c3355c1df6bedc
Jeff Niu <jeffniu22@gmail.com>
2017-03-09 01:21:34 -0500

    autologs if the user has an existing previous session

--- app/src/main/java/com/silpe/vire/slip/MainActivity.java
+++ app/src/main/java/com/silpe/vire/slip/MainActivity.java
@@ -12,6 +12,12 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.silpe.vire.slip.dtos.User;
 import com.silpe.vire.slip.fragments.QRFragment;
 import com.silpe.vire.slip.models.SessionModel;
@@ -24,6 +30,34 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseUser fbUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (fbUser == null) {
+            SessionModel.get().setUser(null);
+            startActivity(new Intent(this, LoginActivity.class));
+        } else {
+            if (SessionModel.get().getUser() == null) {
+                DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+                ref = ref.child(getString(R.string.database_users)).child(fbUser.getUid());
+                ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot snapshot) {
+                        User user = snapshot.getValue(User.class);
+                        SessionModel.get().setUser(user);
+                        construct();
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+
+                    }
+                });
+            } else {
+                construct();
+            }
+        }
+    }
+
+    private void construct() {
         setContentView(R.layout.activity_main);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
 
