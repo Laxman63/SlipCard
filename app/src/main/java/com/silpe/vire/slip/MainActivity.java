ab2fb4001058194f16f04c858917fa05a69fef59
Mogball <jeffniu22@gmail.com>
2017-03-12 12:04:19 -0400

    added transition to account screen

--- app/src/main/java/com/silpe/vire/slip/MainActivity.java
+++ app/src/main/java/com/silpe/vire/slip/MainActivity.java
@@ -140,9 +140,7 @@ public class MainActivity extends AppCompatActivity {
      */
     @Override
     public void onBackPressed() {
-        if (getFragmentManager().getBackStackEntryCount() > 0) {
-            getFragmentManager().popBackStack();
-        }
+        getSupportFragmentManager().popBackStackImmediate();
     }
 
 }
