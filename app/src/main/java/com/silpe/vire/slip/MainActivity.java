d0754e14f1a32a5a572b5d052c9a6d19ae575171
Jeff Niu <jeffniu22@gmail.com>
2017-03-13 23:24:38 -0400

    prototype, rudimentary, manual user adding, considers cases: Cannot add yourself, cannot add nonexisting user, cannot add user already added, etc.

--- app/src/main/java/com/silpe/vire/slip/MainActivity.java
+++ app/src/main/java/com/silpe/vire/slip/MainActivity.java
@@ -13,6 +13,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
--- app/src/main/java/com/silpe/vire/slip/fragments/QRFragment.java
+++ app/src/main/java/com/silpe/vire/slip/fragments/QRFragment.java
@@ -1,19 +1,29 @@
 package com.silpe.vire.slip.fragments;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.silpe.vire.slip.R;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class QRFragment extends Fragment {
     private static final String UID = "mUidView";
 
-    private String uid;
+    private String mUid;
+    private EditText mUidTextField;
 
     public QRFragment() {
     }
@@ -30,7 +40,7 @@ public class QRFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            uid = getArguments().getString(UID);
+            mUid = getArguments().getString(UID);
         }
     }
 
@@ -39,18 +49,85 @@ public class QRFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_qr, container, false);
         TextView uidText = (TextView) view.findViewById(R.id.camera_uid);
-        uidText.setText(uid);
+        uidText.setText(mUid);
 
+        Button addButton = (Button) view.findViewById(R.id.camera_addUser_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doAddUser();
+            }
+        });
+
+        mUidTextField = (EditText) view.findViewById(R.id.camera_inputField);
         return view;
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
+    private void doAddUser() {
+        final String uid = mUidTextField.getText().toString();
+        if (mUid.equals(uid)) {
+            mUidTextField.setError(getContext().getString(R.string.error_add_self));
+            mUidTextField.requestFocus();
+        } else if (TextUtils.isEmpty(uid)) {
+            mUidTextField.setError(getContext().getString(R.string.error_field_required));
+            mUidTextField.requestFocus();
+        } else {
+            final AtomicInteger counter = new AtomicInteger(2);
+            FirebaseDatabase.getInstance()
+                    .getReference()
+                    .child(getContext().getString(R.string.database_users))
+                    .child(uid)
+                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                if (counter.decrementAndGet() == 0) addUid(uid);
+                            } else {
+                                mUidTextField.setError(getContext().getString(R.string.error_invalid_uid));
+                                mUidTextField.requestFocus();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                            mUidTextField.setError(getContext().getString(R.string.error_database));
+                            mUidTextField.requestFocus();
+                        }
+                    });
+            FirebaseDatabase.getInstance()
+                    .getReference()
+                    .child(getContext().getString(R.string.database_connections))
+                    .child(mUid)
+                    .orderByValue()
+                    .equalTo(uid)
+                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.getChildrenCount() > 0) {
+                                mUidTextField.setError(getContext().getString(R.string.error_user_added));
+                                mUidTextField.requestFocus();
+                            } else {
+                                if (counter.decrementAndGet() == 0) addUid(uid);
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                            mUidTextField.setError(getContext().getString(R.string.error_database));
+                            mUidTextField.requestFocus();
+                        }
+                    });
+        }
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
+    private void addUid(String uid) {
+        FirebaseDatabase.getInstance()
+                .getReference()
+                .child(getContext().getString(R.string.database_connections))
+                .child(mUid)
+                .push()
+                .setValue(uid);
+        Toast.makeText(getContext(), R.string.addUser_success, Toast.LENGTH_SHORT);
     }
+
 }
